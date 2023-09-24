51682e31f967a45514aef3405667cff3077434d3
Ryan Stouffer 1 <rstouffer@gmail.com>
2023-09-24 18:43:06 -0400

    added better logic for muliti schedules

--- main.py
+++ main.py
@@ -2,20 +2,48 @@ from croniter import croniter
 from datetime import datetime
 import send, time, os
 
-cts = ['* * * * *', '*/2 * * * *']
+cts = [
+        ['* * * * *','task1'], 
+        ['* * * * *','task2'], 
+        ['* * * * *','task3'], 
+        ['* * * * *','task4'], 
+        ['* * * * *','task5'], 
+        ['* * * * *','task6'], 
+        ['* * * * *','task7'], 
+        ['*/2 * * * *','task8']
+]
 
-iters = []
-nextTimes = []
+#set the next time to max
+nextTime = datetime(2999,12,31,23,59,59)
+#Tasks to run at next time
+ttrant = []
+def getTasks():
+    global nextTime
+    global ttrant 
+    #set the next time to max
+    nextTime = datetime(2999,12,31,23,59,59)
+    for ct in cts:
 
-for ct in cts:
-    iters.append(croniter(ct, datetime.now()))
-    nextTimes.append(iters[-1].get_next(datetime))
+        cron = croniter(ct[0], datetime.now())
+        testTime = cron.get_next(datetime)
+
+        if (testTime < nextTime):
+            print('found newer time', nextTime)
+            nextTime = testTime
+            ttrant = []
+            ttrant.append(ct)
+        elif (testTime == nextTime):
+            print('found same', nextTime)
+            ttrant.append(ct)
+#init tasks
+getTasks()
 
 while True:
-    for i in range(len(nextTimes)):
-        if datetime.now() > nextTimes[i]:
-            nextTimes[i] = iters[i].get_next(datetime)
-            print(nextTimes[i])
+    if nextTime <= datetime.now():
+        for ct in ttrant:
+            print(ct)
+        getTasks()
+        
 
 
 # def job():
