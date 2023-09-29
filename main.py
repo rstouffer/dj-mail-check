bef4169921460918ac372a7559de0817847a5643
Ryan Stouffer 1 <rstouffer@gmail.com>
2023-09-28 20:20:49 -0400

    added connection to DB

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from croniter import croniter
 from datetime import datetime
 import send, time, os
+import mariadb
+import sys
+from dotenv import load_dotenv
 
-cts = [
-        ['* * * * *','task1'], 
-        ['* * * * *','task2'], 
-        ['* * * * *','task3'], 
-        ['* * * * *','task4'], 
-        ['* * * * *','task5'], 
-        ['* * * * *','task6'], 
-        ['* * * * *','task7'], 
-        ['*/2 * * * *','task8']
-]
+load_dotenv()
+
+def getConn():
+    try:
+        return mariadb.connect(
+            user=os.environ.get("DB_USERNAME"),
+            password=os.environ.get("DB_PASSWORD"),
+            host=os.environ.get("DB_HOST"),
+            port=int(os.environ.get("DB_PORT")),
+            database=os.environ.get("DB_DATABASE")
+        )
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB Platform: {e}")
+        sys.exit(1)
 
 #set the next time to max
 nextTime = datetime(2999,12,31,23,59,59)
@@ -20,21 +27,31 @@ ttrant = []
 def getTasks():
     global nextTime
     global ttrant 
+    global conn
+
     #set the next time to max
+
+    conn = getConn()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM cron')
+
     nextTime = datetime(2999,12,31,23,59,59)
-    for ct in cts:
 
-        cron = croniter(ct[0], datetime.now())
+    for row in cursor:
+    
+        cron = croniter(row[1], datetime.now())
+        print('NOW ', datetime.now())
+        print(row[1])
         testTime = cron.get_next(datetime)
-
+        print('TestTime', testTime)
         if (testTime < nextTime):
-            print('found newer time', nextTime)
+            print(testTime, 'newer than', nextTime)
             nextTime = testTime
             ttrant = []
-            ttrant.append(ct)
+            ttrant.append(row)
         elif (testTime == nextTime):
             print('found same', nextTime)
-            ttrant.append(ct)
+            ttrant.append(row)
 #init tasks
 getTasks()
 
